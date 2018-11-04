c5f403e9a4f6d5dd1804b68668cfec4780a650cc
Michael Romer <mvromer@gmail.com>
2018-11-04 15:34:44 -0600

    Added rest of code

--- gen_fib/src/main.rs
+++ gen_fib/src/main.rs
@@ -4,10 +4,29 @@ fn main() {
     println!( "Fibonacci Generator" );
     let fibo_num = read_input();
     println!( "Generating Fibonacci number {}.", fibo_num );
+
+    let result = if fibo_num == 1 || fibo_num == 2 {
+        1
+    } else {
+        let mut result = 0;
+        let mut i = 3;
+        let mut current_fibo_minus_2 = 1;
+        let mut current_fibo_minus_1 = 1;
+        while i <= fibo_num {
+            result = current_fibo_minus_2 + current_fibo_minus_1;
+            current_fibo_minus_2 = current_fibo_minus_1;
+            current_fibo_minus_1 = result;
+            i += 1;
+        }
+
+        result
+    };
+
+    println!( "Result: {}", result );
 }
 
 fn read_input() -> u32 {
-    println!( "Which Fibonacci number do you want to see?" );
+    println!( "Which Fibonacci number (1, 2, ...) do you want to see?" );
     loop {
         let mut fibo_num = String::new();
         io::stdin().read_line( &mut fibo_num )
